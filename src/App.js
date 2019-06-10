dcb1d1582efcd962ddc68346d70a114ee14ee88a
arijit sahoo <tijira1999sahoo@gmail.com>
2019-06-10 16:17:43 +0530

    created board and box

--- src/App.js
+++ src/App.js
@@ -1,12 +1,45 @@
 import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import '.App.css';
+import './App.css';
 
 class App extends React.Component{
+
+    constructor(props)
+    {
+        super(props);
+        this.state= {
+            board: Array(9).fill("X")
+        }
+    }
+
     render() {
+
+        const box = this.state.board.map( box => <div className="box">{box}</div>  )
+
         return(
+            <div className="container">
+
+                <h1>Tic tac toe App</h1> 
+
+                <div className="board">
+                    
+                    <div className="box"></div> 
+                    <div className="box"></div>  
+                    <div className="box"></div>  
+                    <div className="box"></div>  
+                    <div className="box"></div>  
+                    <div className="box"></div>  
+                    <div className="box"></div>  
+                    <div className="box"></div>  
+                    <div className="box"></div> 
+
+                </div> 
+
+            </div> 
             
         )
     }
 
 }
+
+
+export default App;
