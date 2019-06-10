0257751bb209eb20981c7beeea82ce3a6144d1f0
arijit sahoo <tijira1999sahoo@gmail.com>
2019-06-10 17:01:27 +0530

    added event listener with X

--- src/App.js
+++ src/App.js
@@ -11,16 +11,27 @@ class App extends Component{
         }
     }
 
-    handleClick(e){
-        console.log(e.target); 
+    handleClick = (index)=>{
+
+        let newBoard = this.state.board;
+
+        newBoard[index]= 'X';
+
+        this.setState({
+            board: newBoard
+        })
+
+        // this.state.board[index]= 'X';
+        console.log(this.state.board); 
     }
 
     render() {
 
         const box = this.state.board.map( 
             (box,index) => 
-            <div className="box" key={index} 
-            onClick={(e) => this.handleClick(e)}>
+            <div className="box" 
+            key={index} 
+            onClick={() => this.handleClick(index)}>
             {box}
             </div> 
              );
